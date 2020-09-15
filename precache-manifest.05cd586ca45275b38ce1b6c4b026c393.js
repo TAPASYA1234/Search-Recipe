8ceb92d42ec698113059bcc6ca98e8a3951f0c4b
TAPASYA1234 <tapasyaprajapati1234@gmail.com>
2020-09-15 15:32:34 +0530

    Updates

---
+++ precache-manifest.05cd586ca45275b38ce1b6c4b026c393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be11ff2b1006fb0cfaf8bc0cba31ddf",
+    "url": "/Search-Recipe/index.html"
+  },
+  {
+    "revision": "d3f80a4bef7f49d0bb35",
+    "url": "/Search-Recipe/static/css/main.2d3b8e96.chunk.css"
+  },
+  {
+    "revision": "c863836aeb8ea6a24f54",
+    "url": "/Search-Recipe/static/js/2.8e86e149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Search-Recipe/static/js/2.8e86e149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f80a4bef7f49d0bb35",
+    "url": "/Search-Recipe/static/js/main.6ab55dd0.chunk.js"
+  },
+  {
+    "revision": "f7d5aac3a98bed5580b5",
+    "url": "/Search-Recipe/static/js/runtime-main.d63ab539.js"
+  }
+]);
